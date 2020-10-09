62efee138c3226c1edd280714b06400645895250
Nathan Rymarz <nrymarz@gmail.com>
2020-10-09 20:39:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 var kittens = [] //define your array here
 
 // Add your functions and code here
+var kittens = ["Milo, Otis, Garfield"]
+function destructivelyAppendKitten(element){
+  kittens.push(element);
+}
+function destructivelyPrependKitten(kitten){
+  kittens.unshift(kitten);
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(kitten){
+  newKittens = [...kittens,kitten];
+  return newKittens
+}
+function prependKitten(kitten){
+  newKittens = [kitten,...kittens]
+}
